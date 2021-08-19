ad5f1ba797b74325fea6ec65de6ff41b9b4ebc4f
Erick Medrado <j.erick.primo@gmail.com>
2021-08-19 13:20:00 -0300

    Formatacao dos dados exibidos no componente Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <div class="card" v-if="product.address.geo_location && product.address.geo_location.latitude && product.address.geo_location.longitude">
-      <Carousel :images="images"></Carousel>
+      <Carousel :images="product.photos"></Carousel>
       <div class="info">
         <h1 v-html="product.name"></h1>
         <h2 v-html="product.address.district"></h2>
         <h3 v-html="product.address.city"></h3>
         <div class="details">
-          <p><img :src="require('@/assets/images/key.svg')" alt="Ícone chave"> Apartamentos de 97 e 126m²</p>
-          <p><img :src="require('@/assets/images/bed.svg')" alt="Ícone cama"> 3 suítes</p>
-          <p><img :src="require('@/assets/images/pin.svg')" alt="Ícone localização"> Localização privilegiada</p>
+          <p>
+            <img :src="require('@/assets/images/key.svg')" alt="Ícone chave">
+            Apartamentos de 97 e 126m²
+          </p>
+          <p v-if="listSuites">
+            <img :src="require('@/assets/images/bed.svg')" alt="Ícone cama">
+            {{ listSuites }} suítes
+          </p>
+          <p v-if="checkLocal">
+            <img :src="require('@/assets/images/pin.svg')" alt="Ícone localização">
+            Localização privilegiada
+          </p>
         </div>
         <button>Ver mais</button>
       </div>
@@ -30,17 +39,44 @@ export default {
   },
   data () {
     return {
-      data: this.products,
-      images: [
-        'https://www.bild.com.br/uploads/photo/file/13133/PISCINA_01.jpg',
-        'https://www.bild.com.br/uploads/photo/file/13134/PORTICO_DE_ENTRADA.jpg',
-        'https://www.bild.com.br/uploads/photo/file/13135/VARANDA.jpg'
-      ]
+      min_lat: -21.269,
+      min_lng: -47.857,
+      max_lat: -21.211,
+      max_lng: -47.780
     }
   },
-  created: function() {
-    console.log(this.product.address.geo_location)
-  }
+  computed: {
+    checkLocal() {
+      const geo = this.product.address.geo_location
+      if(geo.latitude >= this.min_lat && geo.latitude <= this.max_lat &&
+         geo.longitude >= this.min_lng && geo.longitude <= this.max_lng)
+        return true
+      else
+        return false
+    },
+    sortSuites() {
+      let en_suites = this.product.units.en_suites
+      if (en_suites && en_suites.length) {
+        en_suites = en_suites.filter(suites => suites !== 0)
+        en_suites = en_suites.sort((a, b) => a - b)
+        return en_suites
+      } else
+        return en_suites
+    },
+    listSuites() {
+      let en_suites = this.sortSuites
+      if (en_suites && en_suites.length) {
+        if (en_suites.length > 1) {
+          let str = en_suites.pop()
+          return en_suites.join(', ') + ' e ' + str
+        } else
+          return en_suites.toString()
+      } else if (en_suites && en_suites > 0) {
+        return en_suites
+      } else
+        return false
+    }
+  },
 }
 </script>
 
